b4fd9483bb3d279866ae67b0be98a63eaf78b463
T-Lind <tiernanxklindauer@gmail.com>
2022-12-25 12:03:16 -0600

    main.c updates

--- LEDDimmingUsingPWM/main.c
+++ LEDDimmingUsingPWM/main.c
@@ -1,15 +1,29 @@
 #include <atmel_start.h>
 #include <util/delay.h>
 
+#define SHIFT_AMOUNT 32
+#define SHIFT_MASK ((1 << SHIFT_AMOUNT) - 1)
+
+
 uint32_t dutyCycle = 0;
 
-int main(void)
-{
+int computePi(unsigned int precision) {
+	int piValue = 500 << SHIFT_AMOUNT;
+	
+	for(unsigned int i = 0; i < precision / 2; i++) {
+		
+	}
+	
+	return piValue
+}
+
+int main() {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 
 	/* Replace with your application code */
 	while (1) {
+		
 		for(int i=0;i<TCA1.SINGLE.PER;i++) {
 			dutyCycle = i;
 			TCA1.SINGLE.CMP2 = dutyCycle;
